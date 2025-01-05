363f13199059509a85db510fa5c1335e6fd2f454
maxansari <52211211@nitkkr.ac.in>
2025-01-05 12:28:39 +0530

    commited sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+export const sitemap = {
+    siteUrl: 'https://www.kingswoodpublicschool.in', // Replace with your site's URL
+    generateRobotsTxt: true,          // Generate a robots.txt file
+  changefreq: 'daily',              // Update frequency of the pages
+  priority: 0.7,                    // Default priority of the pages
+  exclude: [],                      // Pages to exclude (if any)
+  sitemapSize: 5000,                // Limit of URLs per sitemap file
+  additionalPaths: async (config) => {
+    return [
+      { loc: '/about', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/contact', changefreq: 'monthly', priority: 0.7 },
+      { loc: '/gallery', changefreq: 'daily', priority: 0.9 },
+    ];
+  },
+  };
+  
